ef7d64ef6a9f137e45c99a1dcae3330101a60552
techygrrrl <techygrrrl@proton.me>
2022-10-11 21:34:44 -0700

    it's a bot

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Node.js TypeScript bootstrapping
+# chatrrr brandrrr
+
+Brands a random chat message with on-brand messaging: 3 R's!
 
 - [Development](#development)
 - [Deployment](#deployment)
@@ -6,13 +8,19 @@
 
 ## Development
 
-Use the right version of Node with [NVM](https://github.com/nvm-sh/nvm):
+It's recommended but not required: Use the right version of Node with [NVM](https://github.com/nvm-sh/nvm):
 
     nvm use
 
 Install the dependencies:
 
     npm install
+
+Create an environment file:
+
+    cp .env.example .env
+
+Configure the environment variables.
 
 Run the dev server:
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,45 @@
-import Express from 'express'
+import * as tmi from "tmi.js";
+import Express from "express";
+import dotenv from "dotenv";
 
-const app = Express()
+dotenv.config();
 
-const port = process.env.PORT || 5006
+const { WATCHED_CHANNEL, BOT_USERNAME, BOT_USER_ACCESS_TOKEN } = process.env;
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'ok' })
-})
+if (!WATCHED_CHANNEL) throw new Error("WATCHED_CHANNEL required");
+if (!BOT_USERNAME) throw new Error("BOT_USERNAME required");
+if (!BOT_USER_ACCESS_TOKEN) throw new Error("BOT_USER_ACCESS_TOKEN required");
 
+const port = process.env.PORT || 4141;
+
+const app = Express();
+
+const client = new tmi.Client({
+  channels: [WATCHED_CHANNEL],
+  identity: {
+    username: BOT_USERNAME,
+    password: `oauth:${BOT_USER_ACCESS_TOKEN}`,
+  },
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, isSelf) => {
+  // Your chatbot logic goes here!
+  const senderUser = tags["display-name"];
+  const modifiedMessage = message.replace(/r/gi, "rrr");
+
+  if (!isSelf) {
+    client.say(
+      channel,
+      `@${senderUser} did you mean: ${modifiedMessage}? Kappa`
+    );
+  }
+});
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
 app.listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`)
-})
+  console.log(`Listening on http://localhost:${port}`);
+});
